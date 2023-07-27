0d943f5f85738b738684cabfa8c9f67207c290fb
ibroxim <ibrohimabdusalomov395@gmail.com>
2023-07-27 12:24:11 +0500

    commit

--- Technoprom/settings.py
+++ Technoprom/settings.py
@@ -1,14 +1,16 @@
 import os
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+env = environ.Env()
+environ.Env.read_env()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xff9odij3e#_s!ruwwuwc=9_m!cucd-atap((_lykp8a@)gvoh'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
